09227380ef2607bfa53c3f85fa678e1af8ec44f5
praveenkumar666 <kumarpraveen92829@gmail.com>
2023-11-29 22:12:04 +0530

    nav bar

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
-console.log("here1");
+import vuetify from "@/plugins/vuetify";
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
@@ -24,12 +24,11 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 getAnalytics(app);
-console.log("here2");
 Vue.config.productionTip = false;
 
 new Vue({
   router,
   store,
+  vuetify,
   render: (h) => h(App),
 }).$mount("#app");
-console.log("here3");
